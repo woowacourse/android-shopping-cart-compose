48d30c2521ec22896bc1685d6b123f2aa3fb92b9
JunJangE <wnswkd486@gmail.com>
2024-10-28 17:24:58 +0900

    refactor: BackButtonTopBar 디자인 수정

--- app/src/main/java/nextstep/shoppingcart/Presentation/component/BackButtonTopBar.kt
+++ app/src/main/java/nextstep/shoppingcart/Presentation/component/BackButtonTopBar.kt
@@ -1,7 +1,6 @@
 package nextstep.shoppingcart.Presentation.component
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,10 +12,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.shoppingcart.Presentation.ui.theme.ShoppingCartTheme
 import nextstep.signup.R
 
 @Composable
@@ -24,7 +27,7 @@ fun BackButtonTopBar(
     modifier: Modifier = Modifier,
     @DrawableRes backIconRes: Int = R.drawable.ic_arrow_back,
     onBack: () -> Unit,
-    @StringRes titleRes: Int,
+    title: String,
 ) {
     Surface(
         modifier =
@@ -40,17 +43,32 @@ fun BackButtonTopBar(
                 Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
             DefaultIconButton(
-                modifier = Modifier.size(24.dp),
+                modifier = Modifier.size(44.dp),
                 iconRes = backIconRes,
                 tint = Color.Black,
                 onClick = onBack,
             )
-            Spacer(modifier = Modifier.width(16.dp))
-            Text(text = stringResource(id = titleRes))
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = title,
+                fontSize = 22.sp,
+            )
         }
     }
 }
 
 private val BACK_BUTTON_TOP_BAR_HEIGHT = 64.dp
+
+@Preview
+@Composable
+private fun BackButtonTopBarPreview() {
+    ShoppingCartTheme {
+        BackButtonTopBar(
+            title = stringResource(id = R.string.product_detail_title),
+            onBack = {},
+        )
+    }
+}
