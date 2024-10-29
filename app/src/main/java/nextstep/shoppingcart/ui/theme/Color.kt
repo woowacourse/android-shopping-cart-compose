6ff7cddb61fcf0c4f565938c186d37ad027a93b1
gaeun5744 <geun5744@gmail.com>
2024-10-29 18:48:30 +0900

    style: 색상 추가

--- app/src/main/java/nextstep/shoppingcart/ui/theme/Color.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/theme/Color.kt
@@ -10,5 +10,8 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val Gray10 = Color(0xFFAAAAAA)
 val Gray20 = Color(0xFF333333)
 val Gray50 = Color(0xFF1D1B20)
+
+val Blue50 = Color(0xFF2196F3)
