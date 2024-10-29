83c6f49251c597e67ead2cc970f0feb96f140c8b
gaeun5744 <geun5744@gmail.com>
2024-10-29 16:47:19 +0900

    refactor: Box -> CenterAlignedTopAppBar로 변경

--- app/src/main/java/nextstep/shoppingcart/ui/productlist/ProductListTopBar.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/productlist/ProductListTopBar.kt
@@ -1,15 +1,16 @@
 package nextstep.shoppingcart.ui.productlist
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,29 +18,31 @@ import androidx.compose.ui.unit.dp
 import nextstep.shoppingcart.ui.theme.Typography
 import nextstep.signup.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductListTopBar(
     onCartClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier =
-            modifier
-                .fillMaxWidth()
-                .padding(vertical = 18.dp, horizontal = 8.dp),
-    ) {
-        IconButton(onCartClick, modifier = Modifier.align(Alignment.CenterEnd)) {
-            Icon(
-                imageVector = Icons.Filled.ShoppingCart,
-                contentDescription = stringResource(R.string.product_list_cart_button_desc),
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                stringResource(R.string.product_list_screen_title),
+                style = Typography.headlineMedium,
+                modifier = Modifier.wrapContentHeight().padding(vertical = 18.dp),
             )
-        }
-        Text(
-            stringResource(R.string.product_list_screen_title),
-            modifier = Modifier.align(Alignment.Center),
-            style = Typography.headlineMedium,
-        )
-    }
+        },
+        actions = {
+            IconButton(onCartClick, modifier = Modifier.padding(end = 8.dp)) {
+                Icon(
+                    imageVector = Icons.Filled.ShoppingCart,
+                    contentDescription = stringResource(R.string.product_list_cart_button_desc),
+                )
+            }
+        },
+        modifier =
+            modifier.fillMaxWidth(),
+    )
 }
 
 @Preview(
