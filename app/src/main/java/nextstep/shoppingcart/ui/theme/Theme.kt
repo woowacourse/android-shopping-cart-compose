0ea7b7c1b71b4c9ea8b72ec98c704c9ca02799bb
gaeun5744 <geun5744@gmail.com>
2024-10-29 12:03:32 +0900

    style: dynamicColor 제거 & 배경 색상 설정

--- app/src/main/java/nextstep/shoppingcart/ui/theme/Theme.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/theme/Theme.kt
@@ -1,20 +1,18 @@
 package nextstep.shoppingcart.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme =
     darkColorScheme(
         primary = Purple80,
         secondary = PurpleGrey80,
         tertiary = Pink80,
+        background = Color.Black,
     )
 
 private val LightColorScheme =
@@ -22,6 +20,7 @@ private val LightColorScheme =
         primary = Purple40,
         secondary = PurpleGrey40,
         tertiary = Pink40,
+        background = Color.White,
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -36,17 +35,10 @@ private val LightColorScheme =
 @Composable
 fun ShoppingCartTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
     val colorScheme =
         when {
-            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-
             darkTheme -> DarkColorScheme
             else -> LightColorScheme
         }
