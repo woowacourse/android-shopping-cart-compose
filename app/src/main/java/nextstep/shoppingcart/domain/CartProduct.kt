71801dd463e6a3e75a226d2b035ec7c0968ac995
gaeun5744 <geun5744@gmail.com>
2024-11-01 10:14:25 +0900

    feat: 전체 가격 계산 로직 구현

--- app/src/main/java/nextstep/shoppingcart/domain/CartProduct.kt
+++ app/src/main/java/nextstep/shoppingcart/domain/CartProduct.kt
@@ -5,6 +5,8 @@ data class CartProduct(
     val quantity: Quantity = Quantity(currentValue = INITIAL_COUNT),
     val countInterval: Int = DEFAULT_COUNT_INTERVAL,
 ) {
+    val totalPrice: Int = product.price * quantity.currentValue
+
     fun increaseCount(): CartQuantityUpdateResult =
         if (quantity.maxValue != null && quantity.currentValue < quantity.maxValue) {
             CartQuantityUpdateResult.Success(
@@ -30,5 +32,9 @@ data class CartProduct(
     companion object {
         private const val DEFAULT_COUNT_INTERVAL = 1
         private const val INITIAL_COUNT = 1
+        val dummy =
+            CartProduct(
+                ProductRepository.dummy.first(),
+            )
     }
 }
