185e9c0bc01bac67990e0396fc7f5f84f9d0044c
gaeun5744 <geun5744@gmail.com>
2024-11-01 10:14:46 +0900

    feat: 최소값보다 작을 경우, 장바구니에서 아이템을 삭제하는 로직 구현

--- app/src/main/java/nextstep/shoppingcart/domain/CartProductRepository.kt
+++ app/src/main/java/nextstep/shoppingcart/domain/CartProductRepository.kt
@@ -1,42 +1,50 @@
 package nextstep.shoppingcart.domain
 
 object CartProductRepository {
-    private val cartItems: MutableList<CartProduct> = mutableListOf()
+    private val _cartItems: MutableList<CartProduct> = mutableListOf()
+    val cartItems: List<CartProduct> get() = _cartItems.toList()
 
     fun addItem(product: Product) {
-        if (cartItems.any { it.product.id == product.id }) {
-            cartItems.add(
+        if (_cartItems.any { it.product.id == product.id }) {
+            _cartItems.add(
                 CartProduct(
                     product = product,
                 ),
             )
             return
         }
-        plusItemCount(product)
+        plusItemCount(product.id)
     }
 
-    fun minusItemCount(product: Product): CartQuantityUpdateResult {
-        val cartProduct = findItem(product.id)
-        val index = cartItems.indexOf(cartProduct)
-        val result = cartProduct.decreaseCount()
-        if (result is CartQuantityUpdateResult.Success) {
-            cartItems[index] = result.product
+    fun minusItemCount(productId: Long) {
+        val cartProduct = findItem(productId)
+        val index = _cartItems.indexOf(cartProduct)
+        when (val result = cartProduct.decreaseCount()) {
+            CartQuantityUpdateResult.MaxFail -> throw IllegalArgumentException("수량 감소시에는 수량 증가 실패 이벤트가 발생할 수 없습니다.")
+            CartQuantityUpdateResult.MinFail -> deleteCartItem(productId)
+            is CartQuantityUpdateResult.Success -> _cartItems[index] = result.product
         }
-        return result
+
     }
 
-    fun plusItemCount(product: Product): CartQuantityUpdateResult {
-        val cartProduct = findItem(product.id)
-        val index = cartItems.indexOf(cartProduct)
+    fun plusItemCount(productId: Long): CartQuantityUpdateResult {
+        val cartProduct = findItem(productId)
+        val index = _cartItems.indexOf(cartProduct)
         val result = cartProduct.increaseCount()
         if (result is CartQuantityUpdateResult.Success) {
-            cartItems[index] = result.product
+            _cartItems[index] = result.product
         }
         return result
+    }
+
+    fun deleteCartItem(productId: Long) {
+        _cartItems.removeIf {
+            it.product.id == productId
+        }
     }
 
     fun findItem(productId: Long): CartProduct =
-        cartItems.firstOrNull {
+        _cartItems.firstOrNull {
             it.product.id == productId
         } ?: throw IllegalArgumentException("장바구니에 $productId 에 대한 상품이 없습니다.")
 }
