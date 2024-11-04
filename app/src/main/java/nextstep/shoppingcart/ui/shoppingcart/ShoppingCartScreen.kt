de191253c20240c9540fb542f3869c1e0a53e470
dpcks0509 <dpcks0509@gmail.com>
2024-11-04 15:53:44 +0900

    refactor: ShoppingCartItem을 Screen 내부로 캡슐화

--- app/src/main/java/nextstep/shoppingcart/ui/shoppingcart/ShoppingCartScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/shoppingcart/ShoppingCartScreen.kt
@@ -1,30 +1,50 @@
 package nextstep.shoppingcart.ui.shoppingcart
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nextstep.shoppingcart.data.repository.DefaultShoppingCartRepository
 import nextstep.shoppingcart.domain.model.ShoppingCartProduct
 import nextstep.shoppingcart.domain.model.ShoppingCartProducts
+import nextstep.shoppingcart.domain.model.products
 import nextstep.shoppingcart.ui.component.BackNavigationTopAppBar
+import nextstep.shoppingcart.ui.component.ProductImage
+import nextstep.shoppingcart.ui.component.QuantityControl
 import nextstep.shoppingcart.ui.component.RectangleButton
-import nextstep.shoppingcart.ui.component.ShoppingCartItem
+import nextstep.shoppingcart.ui.shoppingcart.ShoppingCartAction.MinusQuantity
+import nextstep.shoppingcart.ui.shoppingcart.ShoppingCartAction.PlusQuantity
+import nextstep.shoppingcart.ui.shoppingcart.ShoppingCartAction.RemoveProduct
+import nextstep.shoppingcart.ui.theme.Gray10
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 import nextstep.signup.R
 
@@ -51,9 +71,9 @@ fun ShoppingCartScreen(navigateToBack: () -> Unit) {
     ) { contentPadding ->
         Column(
             modifier =
-            Modifier
-                .fillMaxSize()
-                .padding(contentPadding),
+                Modifier
+                    .fillMaxSize()
+                    .padding(contentPadding),
             verticalArrangement = Arrangement.SpaceBetween,
         ) {
             LazyColumn(
@@ -75,9 +95,9 @@ fun ShoppingCartScreen(navigateToBack: () -> Unit) {
             RectangleButton(
                 text = stringResource(R.string.order_price, shoppingCartProducts.totalPrice),
                 modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .height(54.dp),
+                    Modifier
+                        .fillMaxWidth()
+                        .height(54.dp),
                 onClick = {},
             )
         }
@@ -90,13 +110,13 @@ private fun handleShoppingCartAction(
     updateShoppingCartProducts: (List<ShoppingCartProduct>) -> Unit,
 ) {
     when (action) {
-        is ShoppingCartAction.PlusQuantity ->
+        is PlusQuantity ->
             DefaultShoppingCartRepository.addProduct(shoppingCartProduct.product)
 
-        is ShoppingCartAction.MinusQuantity ->
+        is MinusQuantity ->
             DefaultShoppingCartRepository.decreaseProductQuantity(shoppingCartProduct.product)
 
-        is ShoppingCartAction.RemoveProduct ->
+        is RemoveProduct ->
             DefaultShoppingCartRepository.removeProduct(shoppingCartProduct.product)
     }
     updateShoppingCartProducts(DefaultShoppingCartRepository.shoppingCartProducts)
@@ -104,8 +124,101 @@ private fun handleShoppingCartAction(
 
 @Composable
 @Preview(showBackground = true)
-fun ShoppingCartScreenPreview() {
+private fun ShoppingCartScreenPreview() {
     ShoppingCartTheme {
         ShoppingCartScreen(navigateToBack = {})
+    }
+}
+
+@Composable
+private fun ShoppingCartItem(
+    shoppingCartProduct: ShoppingCartProduct,
+    action: (ShoppingCartAction) -> Unit,
+) {
+    OutlinedCard(
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 18.dp, vertical = 16.dp),
+        shape = RoundedCornerShape(4.dp),
+        border = BorderStroke(1.dp, Gray10),
+    ) {
+        Row(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(start = 18.dp, end = 18.dp, top = 18.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = shoppingCartProduct.product.name,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold,
+            )
+
+            IconButton(
+                onClick = {
+                    action(RemoveProduct(product = shoppingCartProduct.product))
+                },
+                modifier = Modifier.size(24.dp),
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = stringResource(R.string.remove_shopping_cart_item),
+                )
+            }
+        }
+
+        Row(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 18.dp,
+                        end = 18.dp,
+                        top = 6.dp,
+                        bottom = 18.dp,
+                    ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            ProductImage(
+                product = shoppingCartProduct.product,
+                modifier = Modifier.size(width = 136.dp, height = 84.dp),
+                contentScale = ContentScale.FillWidth,
+            )
+
+            Column(modifier = Modifier.align(Alignment.Bottom)) {
+                Text(
+                    text = stringResource(R.string.price_format, shoppingCartProduct.totalPrice),
+                    modifier =
+                        Modifier
+                            .align(Alignment.End),
+                    fontSize = 16.sp,
+                )
+
+                QuantityControl(
+                    modifier = Modifier.size(42.dp),
+                    quantity = shoppingCartProduct.quantity,
+                    minusQuantity = { action(MinusQuantity(product = shoppingCartProduct.product)) },
+                    plusQuantity = { action(PlusQuantity(product = shoppingCartProduct.product)) },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun ShoppingCartItemPreview() {
+    ShoppingCartTheme {
+        ShoppingCartItem(
+            shoppingCartProduct =
+                ShoppingCartProduct(
+                    products.first(),
+                    quantity = 2,
+                ),
+            action = {},
+        )
     }
 }
